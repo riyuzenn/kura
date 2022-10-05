fdd6afe416df13f4622611f8736fca56442669a0
riyuzenn <riyuzenn@gmail.com>
2022-10-05 04:24:38 +0200

    ✨ feat: create kura library

--- crates/kura/src/lib.rs
+++ crates/kura/src/lib.rs
@@ -16,17 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub use self::face::Face;
+pub use self::filter::KuraFilter;
+mod filter;
+mod face;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
